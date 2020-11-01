0ab00fda9623bd49b0b8ee435c5affa5abbc264b
shamarnelson <50060085+shamarnelson@users.noreply.github.com>
2020-10-31 22:45:53 -0500

    Edited js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+
+function submission(){
+    var validation=/\S+@\S+\.\S+/;
+    var inputn= submission.test(document.getElementById("email").value);
+    var sub=document.getElementById("email").value;
+
+    if(!inputn||inputn==null){
+        alert("PLease enter a vaild email address.");
+    }
+    else{
+        alert{"Thank you! Your email address" +" "+ val +" has been to our mailing list!");
+    
+    }
+
+    }
+    return inputn
+}
